ddfff85a57ddf982d3543f2b47a8eda1bfe8dcd8
Moibe <moi_estrello@hotmail.com>
2022-08-06 03:45:35 -0500

    OpenStreet to https

--- divrows.js
+++ divrows.js
@@ -52,7 +52,7 @@ let mapOptions = {
 let map = new L.map('map' , mapOptions, { zoomControl:false });
 
 
-let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+let layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
 //Icono:
