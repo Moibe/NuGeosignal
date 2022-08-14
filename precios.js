78636d66f7f1ede8c871687132d9d7a2f1e77201
Moibe <moi_estrello@hotmail.com>
2022-08-14 16:31:23 -0500

    Prices-Languages Ok!

--- precios.js
+++ precios.js
@@ -1,5 +1,7 @@
 //Declaramos el json con las variables de idioma.
 let jsonPrecios = JSON.parse(precios);
+//Idioma viene cortado de la url, es decir que por ejemplo EU aunque no es un idioma si nos ayuda a definir...
+//el precio de Europa porque esa es la URL que viene en la Campaña.
 idd_now = jsonPrecios[idioma].idd;
 console.log("El idd reportado desde precios.js es:");
 console.log(idd_now);
