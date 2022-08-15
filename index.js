226839879189d0b67c9a498f2bb7e0a934ac985d
Moibe <moi_estrello@hotmail.com>
2022-08-14 23:44:50 -0500

    Removed init.js

--- index.js
+++ index.js
@@ -3,7 +3,59 @@ inicializador();
 console.log("Ejecutabdo variables_iniciales();")
 variables_iniciales();
 
-const todo = document.getElementById('todo');
+
+function inicializador(){
+
+    //Idioma
+    let idioma; 
+    let producto;
+    
+    //El idioma defualt es el idioma que tendremos en local 127.0.0.1 porque no hay un subdominio que lo designe.
+    let idioma_default = "en";
+    console.log("El idioma default es:" + idioma_default);
+    //El producto es el que le dará el precio y link de lo que estamos vendiendo que no siempre está ligado a un idioma a...
+    //traducir.
+    let producto_default = "en";
+    console.log("El producto default es:" + producto_default);
+    
+    //Se revisa el hostname para saber si estamos en local o online.
+    const hostname = window.location.hostname;
+    console.log("Esto es hostname:");
+    console.log(hostname);
+    if(hostname == "127.0.0.1"){
+        console.log("Estamos en local...");
+        idioma = idioma_default;
+        console.log("El idioma será:" + idioma);
+        producto = producto_default;
+        console.log("El producto será:" + producto);
+    }
+    else{
+        //Si estamos en producción el idioma lo definimos desde la url.
+        console.log("Entramos a chequeo de path...");
+        const pathname = window.location.pathname;
+        let pathcut = pathname.replace(/\//g,''); // Remove all slashes from string
+        if (pathcut == ""){
+            console.log("pathcut está vacío...");
+            //Si no hay patchcut entonces el idioma default será inglés.
+            //Cambiar después a que lo guarde en una cookie, por ahora sin cookies hasta primer revisión.
+            idioma = idioma_default;
+            console.log("El idioma será:" + idioma);
+            producto = producto_default;
+            console.log("El producto será:" + producto);
+        }else{
+            console.log("Esto es pathcut: " + pathcut);
+            //El idioma se usa para las traducciones.
+            idioma = pathcut;
+            console.log("El idioma será:" + idioma);
+            //El producto se usa para los precios dependiendo de la Campaña y País.
+            producto = pathcut;
+            console.log("El producto será:" + producto);
+        }
+    }
+    
+    
+    }
+    
 
 function firstCheck(){
     referido = document.referrer;
@@ -53,13 +105,13 @@ function redireccionador(){
     }
     
     window.location.assign(asignacion);
-    //newDoc("/divrows.html");
 
  }
 
 function variables_iniciales(){
 
-    let paso = 1; 
+const todo = document.getElementById('todo');
+let paso = 1; 
 let marker_inicial;
 var circle;
 var maxPoints = 10;
@@ -93,11 +145,6 @@ const seccionQuery = document.getElementById('query');
 const tel_field = document.getElementById('tel_field');
 const mensajes = document.getElementById("mensajes");
 const locate_sample = document.getElementById("locate_sample");
-
-//Inicialiación del botón Principal.
-const btnSubmit = document.getElementById('btnSubmit');
-btnSubmit.value = btnSubmit_text;
-btnSubmit.addEventListener('click', startProcess);
 
 var iconFile = 'ico-cel.png';
 
@@ -138,8 +185,10 @@ map.boxZoom.disable();
 map.keyboard.disable();
 map.zoomControl.disable(); */
 
-
-
+//Inicialiación del botón Principal.
+const btnSubmit = document.getElementById('btnSubmit');
+btnSubmit.value = btnSubmit_text;
+btnSubmit.addEventListener('click', startProcess);
 }
  
 function startProcess(){
