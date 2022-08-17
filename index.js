88d27e0a2cb5c0f55d2d7c37a77c0391cb8b58da
Moibe <moi_estrello@hotmail.com>
2022-08-16 19:32:48 -0500

    Try Catch Finally

--- index.js
+++ index.js
@@ -153,9 +153,16 @@ function setPrecios(){
     let jsonPrecios = JSON.parse(precios);
     //Idioma viene cortado de la url, es decir que por ejemplo EU aunque no es un idioma si nos ayuda a definir...
     //el precio de Europa porque esa es la URL que viene en la Campaña.
-    idd_product = jsonPrecios[idioma].idd;
-    console.log("El idd reportado desde precios.js es:");
-    console.log(idd_product);
+
+    try {
+        idd_product = jsonPrecios[idioma].idd;
+        console.log("El idd reportado desde precios.js es:");
+        console.log(idd_product);
+      } catch (ex) {
+        console.error('inner', ex.message);
+      } finally {
+        console.log('finally');
+
 
 }
 
