e7c22b9eda1ebd046ce8b65f8ad0a013c86e25ce
Moibe <moi_estrello@hotmail.com>
2022-12-10 17:50:47 -0600

    Dupliqué handlePermissions()

--- index.js
+++ index.js
@@ -389,6 +389,40 @@ function busquedaPaso1(){
 
 }
 
+function handlePermission() {
+    console.log("Entré a Handle Permissions...");
+      navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+        if (result.state === 'granted') {
+          report(result.state);
+          permiso = result.state;
+          locate_sample.style.display = 'none';
+        } else if (result.state === 'prompt') {
+          report(result.state);
+          permiso = result.state;
+          //geoBtn.style.display = 'none';
+          //navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+        } else if (result.state === 'denied') {
+          report(result.state);
+          permiso = result.state;
+          //geoBtn.style.display = 'inline';
+        }
+        result.addEventListener('change', () => {
+          report(result.state);
+          permiso = result.state;
+          if(window_aceptar_permiso == true && permiso == "granted"){
+              locate_sample.style.display = 'none';
+              registrarPosicion();
+          }else{
+              console.log("No haré nada ante el cambio de permiso...");
+          }
+        });
+      });
+    }
+    
+    function report(state) {
+      console.log(`Permission ${state}`);
+    }
+
 function busquedaPaso2(){
     
     window_aceptar_permiso = false;
