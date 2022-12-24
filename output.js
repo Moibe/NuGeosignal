46ed4c4c78248625048d44607e2b3e62f14f92aa
Moibe <moi_estrello@hotmail.com>
2022-12-23 19:20:24 -0600

    Fix output

--- output.js
+++ output.js
@@ -18,14 +18,14 @@ if (document.referrer == ''){
 
     console.log("El document referrer está vacío.");
     referido = 'vacio';
-    localStorage.setItem(document.referrer, JSON.stringify('referido_stored'));
+    localStorage.setItem('referido_stored', JSON.stringify(document.referrer));
 
 }
 else{
     console.log("No está vacío el referrer.");
     console.log("El referrer es:");
     console.log(document.referrer);
-    localStorage.setItem(document.referrer, JSON.stringify('referido_stored'));
+    localStorage.setItem('referido_stored', JSON.stringify(document.referrer));
 }
 
 const pathname = window.location.pathname;
